116ba74c40dcedd903ecaac65261cca988dd59a4
James Bergstra <james.bergstra@gmail.com>
2013-10-07 17:09:19 -0400

    make CPU affinity hack optional

--- hpnnet/__init__.py
+++ hpnnet/__init__.py
@@ -3,6 +3,9 @@ import os
 import psutil
 p = psutil.Process(os.getpid())
 all_cpus = list(range(psutil.NUM_CPUS))
-if p.get_cpu_affinity() != all_cpus:
-    print 'Setting CPU AFFINITY to %s' % str(all_cpus)
-    p.set_cpu_affinity(all_cpus)
+try:
+    if p.get_cpu_affinity() != all_cpus:
+        print 'Setting CPU AFFINITY to %s' % str(all_cpus)
+        p.set_cpu_affinity(all_cpus)
+except:
+    pass
